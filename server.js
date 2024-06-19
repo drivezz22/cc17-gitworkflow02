ab0699eb500ba97790e205298326d2e7ff84b900
drivezz22 <puwanut.pansailom@gmail.com>
2024-06-19 12:25:15 +0700

    feat : add user router

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const notFound = require("./middlewares/not-found");
+const userRouter = require("./routes/user-route");
 const app = express();
 
+app.use("/users", userRouter);
 app.use(notFound);
 
 const port = 8000;
