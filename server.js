ae91220089ec3f42985e880dc6164ce034178647
drivezz22 <puwanut.pansailom@gmail.com>
2024-06-19 12:19:21 +0700

    feat : add not found middleware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const notFound = require("./middlewares/not-found");
 const app = express();
+
+app.use(notFound);
 
 const port = 8000;
 app.listen(port, () => console.log("Server on ", port));
