6f7c1021c5f2334929bd2a48dc88a74aa660d282
WorakornUk <worakorn.uk@gmail.com>
2024-06-19 12:40:13 +0700

    feat : add error-middleware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const errorMiddleware = require("./middlewares/error-middleware");
 const app = express();
+
+app.use(errorMiddleware)
 
 const port = 8000;
 app.listen(port, () => console.log("Server on ", port));
